3f41a56523e3b85686b8cd6f732c1675351f6c4c
Arkady Vinkovsky <66293881+vinkovsky@users.noreply.github.com>
2021-12-07 00:16:38 +0700

    fix search

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -4,35 +4,40 @@ import "./AppHeader.less";
 
 const AppHeader = () => {
   const [state, setState] = useContext(AppContext);
-  const [text, setText] = useState();
+  const [value, setValue] = useState("");
 
   const onClickHandler = () => {
     setState({ ...state, current: state.initial });
   };
 
   const checkboxHandler = ({ target: { checked } }) => {
+    const current = checked
+      ? state.current.filter((item) => !item.online)
+      : state.initial;
+
     setState({
       ...state,
-      current: checked
-        ? state.current.filter((item) => !item.online)
-        : state.initial,
+      current,
     });
   };
 
   const inputHandler = ({ target: { value } }) => {
-    setText(value);
+    setValue(value);
+
+    const current = [...state.initial].filter((item) =>
+      item.name.toLowerCase().includes(value)
+    );
+
     setState({
       ...state,
-      current: state.current.filter((item) =>
-        item.name.toLowerCase().includes(value)
-      ),
+      current,
     });
   };
 
   return (
     <div className="app-header">
       <span>Имя</span>
-      <input type="text" value={text} onChange={(e) => inputHandler(e)} />
+      <input type="text" value={value} onChange={(e) => inputHandler(e)} />
       <div>
         <span>Онлайн</span>
         <input type="checkbox" onChange={(e) => checkboxHandler(e)} />
--- src/components/TableRow/TableRow.jsx
+++ src/components/TableRow/TableRow.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { players } from "../../config/players";
 import { AppContext } from "../AppContainer/AppContainer";
 import "./TableRow.less";
 
@@ -7,28 +6,21 @@ const TableRow = () => {
   const [state, setState] = useContext(AppContext);
 
   const onClickHandler = (id) => {
-    const elem = state.current.filter((item) => item.id === id);
-    const list = state.current.filter((item) => item.id !== id);
-    setState({ ...state, current: list });
+    const current = state.current.filter((item) => item.id !== id);
+    setState({ ...state, current });
   };
 
   return state.current.map(({ id, name, level, online }, i) => (
     <tr className="table-row" key={name + i} onClick={() => onClickHandler(id)}>
+      <td>{id}</td>
+      <td>{name}</td>
       <td>
-        <span>{id}</span>
+        {[...new Array(level)].map((_, i) => (
+          <i className="star" key={i} />
+        ))}
       </td>
       <td>
-        <span>{name}</span>
-      </td>
-      <td>
-        <span>
-          {[...new Array(level)].map((_, i) => (
-            <i className="star" key={i} />
-          ))}
-        </span>
-      </td>
-      <td>
-        <span className={online ? "indicator online" : "indicator"}></span>
+        <span className={online ? "indicator online" : "indicator"} />
       </td>
     </tr>
   ));
